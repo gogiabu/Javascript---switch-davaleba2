7a42c79e5bada4e72b9d22640ae273567bcd0fa2
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-04-26 11:53:30 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var string = "dge";
+
+switch(string) {
+
+    case 'weli':
+    document.write(1945);
+    case 'saukune':
+        document.write(20);
+    case 'saati':
+        document.write(1);
+    case 'kvira':
+        document.write("ორშაბათი");
+    default:
+        document.write("არასწორია");
+}
