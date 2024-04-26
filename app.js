40f5c223b5eab816729aa1cd54a5a85f602b985e
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-04-26 03:52:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var string = "dge";
 switch(string) {
 
     case 'weli':
-    document.write(1945);
+        document.write(1945);
+        break;
     case 'saukune':
         document.write(20);
+        break;
     case 'saati':
         document.write(1);
+        break;
     case 'kvira':
         document.write("ორშაბათი");
+        break;
     default:
         document.write("არასწორია");
 }
